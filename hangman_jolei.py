877017ff4b3148f8f617a8a9cfb93ddb701690e7
Jolei <djoleife@gmail.com>
2025-03-04 07:37:21 +0800

    [BUG] while condition not working

--- hangman_jolei.py
+++ hangman_jolei.py
@@ -11,3 +11,16 @@ num_try = 6
 
 print("Welcome to Hangman Game!")
 
+while num_try > 0 and '_' in guess_word:
+    print("\nrandom_word:", ''.join(guess_word))
+    print(f"You have {num_try} attempts left")
+    guess_try = input("Please guess a letter:").lower()
+
+    if guess_try in random_word:
+        for i in range(len(random_word)):
+            if random_word[i] == guess_try:
+                guess_word[i] = guess_try
+    else:
+        tries = -1
+        print(f"Wrong guess!")
+
