966b80ff16702357da7072edbb1d506c3fd1cb37
Jolei <djoleife@gmail.com>
2025-03-04 08:58:31 +0800

    [FIX] add User's guess letter storage

--- hangman_jolei.py
+++ hangman_jolei.py
@@ -8,24 +8,28 @@ print(random_word)
 
 guess_word = ['_']* len(random_word)
 num_try = 6
+user_guess = [] # store the user input
 
 print("Welcome to Hangman Game!")
 
 while num_try > 0  and '_' in guess_word:
     print("\nWord:", ''.join(guess_word))
     print(f"You have {num_try} attempts left")
+    print("User's guess:", ','.join(user_guess))
     guess_try = input("Please guess a letter: ").lower()
+    if guess_try not in user_guess:
+        user_guess.append(guess_try)
 
-    if guess_try in random_word:
+    if guess_try in random_word.lower():
         for i in range(len(random_word)):
-            if random_word[i] == guess_try:
-                guess_word[i] = guess_try
+            if random_word[i].lower() == guess_try:
+                guess_word[i] = random_word[i]
         print("You guessed it right!")
     else:
         num_try -= 1
         print(f"Wrong guess!")
 
 if '_' not in guess_word:
-    print("You guessed the word!")
+    print(f"You guessed the word! {random_word}")
 else:
     print(f"Game over! The word is : {random_word}")
