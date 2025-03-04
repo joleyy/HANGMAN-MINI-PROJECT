8a695aabc03801389008bef5f5e8033f71419baf
Jolei <djoleife@gmail.com>
2025-03-04 08:00:36 +0800

    [BUG] code is printing game over right away

--- hangman_jolei.py
+++ hangman_jolei.py
@@ -7,20 +7,25 @@ random_word = random.choices(set_words)
 print(random_word)
 
 guess_word = ['_']* len(random_word)
-num_try = 6
+num_try = 0
 
 print("Welcome to Hangman Game!")
 
-while num_try > 0 and '_' in guess_word:
-    print("\nrandom_word:", ''.join(guess_word))
+while num_try > 0  and '_' in guess_word:
+    print("\nWord:", ''.join(guess_word))
     print(f"You have {num_try} attempts left")
-    guess_try = input("Please guess a letter:").lower()
+    guess_try = input("Please guess a letter: ").lower()
 
     if guess_try in random_word:
         for i in range(len(random_word)):
             if random_word[i] == guess_try:
                 guess_word[i] = guess_try
+        print("You guessed it right!")
     else:
-        tries = -1
+        num_try -= 1
         print(f"Wrong guess!")
 
+if '_' not in guess_word:
+    print("You guessed the word!")
+else:
+    print(f"Game over! The word is : {random_word}")
