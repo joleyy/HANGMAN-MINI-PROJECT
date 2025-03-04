f93ba1b82bbb499026d7d8e119cbb71ba51fe280
Jolei <djoleife@gmail.com>
2025-03-04 08:06:54 +0800

    [BUG] if condition not working

--- hangman_jolei.py
+++ hangman_jolei.py
@@ -3,11 +3,11 @@ import random
 set_words = ["Python", "Pandas", "Numpy", "SQL", "Repository", "Git", "Github"]
 
 
-random_word = random.choices(set_words)
+random_word = random.choice(set_words)
 print(random_word)
 
 guess_word = ['_']* len(random_word)
-num_try = 0
+num_try = 6
 
 print("Welcome to Hangman Game!")
 
